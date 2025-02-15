0db61c30755c7fd4bda0e61fb89d9dc46e9e0c75
echooed <echoechodeveloper@gmail.com>
2025-02-15 11:01:56 +0100

    config: set up docker container

---
+++ dags/reddit_dag.py
@@ -0,0 +1,8 @@
+import os
+import sys
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
