9df4986d0ba7778fddc0e2f074c996d617ed46a6
echooed <echoechodeveloper@gmail.com>
2025-02-15 11:35:52 +0100

    feat(pipeline): add airflow DAG for reddit api ingestion

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -1,8 +1,48 @@
 import os
 import sys
 from datetime import datetime
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from reddit_pipeline import reddit_pipeline
+from upload_s3_pipeline import upload_s3_pipeline
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+default_args = {
+    'owner': 'Michael Akindele',
+    'start_date': datetime(2025, 1, 15),
+    'retries': 2,
+}
+
+file_postfix = datetime.now().strftime('%Y%m%d%H%M%S')
+
+dag = DAG(
+    dag_id='etl_reddit_pipeline_dag',
+    default_args=default_args,
+    description='DAG to extract, transform and load Reddit data',  
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['reddit', 'etl', 'pipeline']
+)
+
+# extraction from reddit
+extract = PythonOperator(
+    task_id='reddit_extract',
+    python_callable=reddit_pipeline,
+    op_kwargs={
+        'file_name': f'reddit_{file_postfix}',
+        'subreddit': 'dataengineering',
+        'time_filter': 'day',
+        'limit': 100
+    },
+    dag=dag
+)
+
+# upload to s3
+upload_s3 = PythonOperator(
+    task_id='s3_upload',
+    python_callable=upload_s3_pipeline,
+    dag=dag
+)
+
+extract >> upload_s3
---
+++ pipelines/reddit_pipeline.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from etls.reddit_etl import connect_reddit, extract_posts, load_data_to_csv
+from utils.constants import CLIENT_ID, SECRET, OUTPUT_PATH
+
+def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=None) -> str:
+    """
+    Reddit ETL pipeline: Connect, extract, transform, and save posts to CSV.
+    """
+    try:
+        # Connect to Reddit
+        reddit_instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
+        
+        # Extract posts
+        posts = extract_posts(reddit_instance, subreddit, time_filter, limit)
+        
+        # Transform posts into DataFrame
+        posts_df = pd.DataFrame(posts)
+        
+        # Save DataFrame to CSV
+        file_path = f'{OUTPUT_PATH}/{file_name}.csv'
+        load_data_to_csv(posts_df, file_path)
+        
+        return file_path
+
+    except Exception as e:
+        print(f"Error in Reddit pipeline: {e}")
+        raise
